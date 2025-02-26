92a126d5dca08ae7b16855d94343648e89d058b7
amhart02 <har20010@byui.edu>
2025-02-25 18:21:53 -0700

    Made the hamburger menu close on "scroll" Added his social media links

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.querySelector(".menu-toggle");
     const navMenu = document.querySelector(".nav-menu");
   
-    menuToggle.addEventListener("click", () => {
-      menuToggle.classList.toggle("active");
-      navMenu.style.display =
-        navMenu.style.display === "block" ? "none" : "block";
-    });
-  });
-  
+    window.addEventListener("scroll", closeMenu);
+    menuToggle.addEventListener("click", menuToggleFunction);
+
+  function menuToggleFunction () {
+    menuToggle.classList.toggle("active");
+    navMenu.style.display === "block" ? "none" : "block";
+  }
+
+  function closeMenu () {
+    menuToggle.classList.remove("active");
+  }
+})
